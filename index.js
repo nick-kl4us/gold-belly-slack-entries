b25bbbea35817672d868b87fb19045c6a6adea5a
klaus <nick.gentile@tatari.tv>
2021-12-20 21:40:06 -0500

    add "nums left" array log

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const fs = require("fs");
 const papa = require("papaparse");
 const file = fs.createReadStream("./answers.csv");
-var count = 0; // cache the running count
+let count = 0; // cache the running count
 
 const entries = new Map([]);
 const pickedNumbers = new Set([]);
 const pickAgainPeople = new Set([]);
+
+const MAX = 1000;
+const availableNumbersMap = getAvailableNumbersMap();
+
+function getAvailableNumbersMap() {
+  const numMap = new Map([]);
+  for (let i = 1; i <= MAX; i++) {
+    numMap.set(i, true);
+  }
+  return numMap;
+}
 
 papa.parse(file, {
   worker: true,
@@ -30,6 +41,7 @@ papa.parse(file, {
           pickAgainPeople.add(user);
         } else {
           pickedNumbers.add(cleanEntry);
+          availableNumbersMap.delete(cleanEntry);
           entries.set(user, cleanEntry);
         }
       } else {
@@ -41,5 +53,7 @@ papa.parse(file, {
     console.log("parsing complete read", count - 1, "records.");
     console.log("Gold Belly gift card entries...\n", entries);
     console.log("People that need to pick again:\n", pickAgainPeople);
+    console.log("The numbers that are left!:\n", JSON.stringify(Array.from(availableNumbersMap.keys())));
+    console.log("END...");
   },
 });
